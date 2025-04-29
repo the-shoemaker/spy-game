b3a92de64fde0fabe12f6b6545205687541c0cf0
Dan Schumacher <d.schumacher@p-flow.ch>
2025-04-29 10:04:03 +0200

    init

---
+++ topics.js
@@ -0,0 +1,41 @@
+const topics = {
+    orte: [
+      "Schweiz", "Frankreich", "Deutschland", "Italien", "Spanien",
+      "Kanada", "Japan", "Australien", "Brasilien", "Südafrika",
+      "Mexiko", "Indien", "Russland", "Ägypten", "Norwegen"
+    ],
+    staedte: [
+      "Zürich", "Genf", "Bern", "Basel", "Lausanne",
+      "New York", "Tokio", "London", "Paris", "Hongkong",
+      "Berlin", "Rom", "Moskau", "Dubai"
+    ],
+    gerichte: [
+      "Fondue", "Raclette", "Pizza", "Sushi", "Burger",
+      "Pasta", "Curry", "Steak",
+      "Teigtaschen", "Kebab"
+    ],
+    situationen: [
+      "Erstes Date", "Vorstellungsgespräch",
+      "Familienessen", "Im Stau stehen",
+      "Öffentliches Reden", "Urlaubsplanung", "Konzertbesuch",
+      "Zug verpassen", "Zu spät zu einem Meeting kommen",
+    ],
+    sport: [
+      "Fussball", "Basketball", "Tennis",
+      "Eishockey", "Golf", "Schwimmen", "Radfahren",
+      "Volleyball", "Tischtennis", "Badminton", "Laufen", "Boxen"
+    ],
+    filme: [
+      "Kung Fu Panda", "Titanic", "Die göttliche Ordnung", "Honig im Kopf", "Inside Out"
+    ],
+    tiere: [
+      "Löwe", "Elefant", "Giraffe", "Känguru",
+      "Pinguin", "Delfin", "Panda", "Adler", "Zebra",
+      "Bär", "Wolf", "Fuchs", "Kaninchen", "Affe"
+    ],
+    hobbys: [
+      "Lesen", "Gartenarbeit", "Kochen", "Wandern", "Fotografie",
+      "Malen", "Tanzen", "Angeln", "Schreiben",
+      "Stricken", "Gaming", "Vogelbeobachtung", "Reisen", "Yoga"
+    ]
+  };
